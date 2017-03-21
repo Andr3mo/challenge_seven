1dc415d85940c4967ab1ff6bf9047a100ed8c1ab
Anderson <andersonpirespereira@hotmail.com>
2017-03-21 06:00:23 -0500

    Part 2 - Added the finder controller and views, loaded the views using the _customer partial and added the necessary css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'customers/alphabetized' => 'finder#alphabetized'
+
+  get 'customers/missing_email' => 'finder#missing_email'
+
+  get 'customers' => 'finder#index'
+
+  root to: 'finder#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
